ccfd01c0e0b17d8401891ddb9bbd6fd84d0ea096
SebiCF <55686742+SebiCF@users.noreply.github.com>
2019-10-05 16:51:53 +0200

    Finishes CodeReview02

--- js/script.js
+++ js/script.js
@@ -1,24 +1,46 @@
-function calculateInsurance(){
-	let name = document.getElementById("name").value;
-	let age = parseInt(document.getElementById("age").value);
-	let country = document.getElementById("country").value;
-	let hp = parseInt(document.getElementById("horsePower").value);
-	let insurance;
-
-	if (country == "Austria"){
-		insurance = hp * 100 / age + 50;
-	}
-	else if(country == "Hungary"){
-		insurance = hp * 120 / age + 100;
-	}
-	else if(country == "Greece"){
-		insurance = hp * 150 / (age+3) + 50;
-	}
-
-	document.getElementById("calcResult").innerHTML = name+", your insurance costs "+insurance+"€";
+window.onload = function() { /* enables enter on press to call function*/
+    document.getElementById("horsePower").addEventListener("keydown", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("onEnter").click();
+        }
+    });
 }
 
+function calculateInsurance() {
+    let name = document.getElementById("name").value;
+    let age = parseInt(document.getElementById("age").value);
+    let country = document.getElementById("country").value;
+    let hp = parseInt(document.getElementById("horsePower").value);
+    let insurance;
+    document.getElementById("calcResult").style.visibility = "visible";
+    try {
+        if (name === "") throw "Name ";
+    } catch (err) {
+        document.getElementById("calcResult").innerHTML = err + "can not be empty!";
+    }
 
+    if (country == "Austria") {
+        insurance = hp * 100 / age + 50;
+    } else if (country == "Hungary") {
+        insurance = hp * 120 / age + 100;
+    } else if (country == "Greece") {
+        insurance = hp * 150 / (age + 3) + 50;
+    }
+    let uppername = capitalizeFirstChar(name);
+    document.getElementById("calcResult").innerHTML = uppername + ", your insurance costs " + insurance + "€.";
+
+    try {
+        if (isNaN(insurance)) throw "Age or Horsepower ";
+    } catch (err) {
+        document.getElementById("calcResult").innerHTML = err + "can not be empty!";
+    }
+}
+
+function capitalizeFirstChar(name) {
+    uppername = name[0].toUpperCase() + name.substring(1);
+    return uppername;
+}
 
 
 
